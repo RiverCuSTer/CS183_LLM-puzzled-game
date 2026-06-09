dbef5eb69a05db8d732f8d67efd6ae07f409649a
HL-Ning <1759958360@qq.com>
2026-06-09 12:44:12 +0800

    “l3”

--- Assets/level3(deserted code)25125851/SceneManagerHelper.cs
+++ Assets/level3(deserted code)25125851/SceneManagerHelper.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+using UnityEngine;//   25125851  chuzhaoning
 using UnityEngine.SceneManagement;
 
 public class SceneManagerHelper : MonoBehaviour
--- Assets/level3(deserted code)25125851/SlotCheck.cs
+++ Assets/level3(deserted code)25125851/SlotCheck.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+using UnityEngine;//   25125851  chuzhaoning
 using TMPro;
 
 public class SlotCheck : MonoBehaviour
